79066a274954d8120422b4da0481ea36b498b397
Birdium <bibishu2001@163.com>
2023-09-11 10:55:05 +0800

    assign3

--- assign3/program/src/karel_impl.ml
+++ assign3/program/src/karel_impl.ml
@@ -38,7 +38,20 @@ let set_cell (grid : grid) ((i, j) : pos) (cell : cell) : grid =
 ;;
 
 let state_to_string (state : state) : string =
-  raise Unimplemented
+  (* raise Unimplemented *)
+  let chgrid = 
+    List.mapi state.grid ~f:(fun (i : int) (lst : cell list) : string list -> 
+      List.mapi lst ~f:(fun (j : int) (c : cell) : string -> 
+        if (i, j) = state.karel_pos then 
+          "K"
+        else 
+          match c with
+          | Empty -> "."
+          | Wall -> "x" 
+          | Beeper -> "B"))
+  in 
+  let slist = List.map chgrid ~f:(fun (row : string list) -> String.concat ~sep:" " row) in 
+  String.concat ~sep:"\n" slist
 ;;
 
 let empty_grid (m : int) (n : int) : grid =
@@ -95,11 +108,75 @@ and instruction_list_to_string (instrs: instruction list) : string =
 
 
 let rec eval_pred (state : state) (pred : predicate) : bool =
-  raise Unimplemented
+  (* raise Unimplemented *)
+  match pred with
+  | FrontIs cell -> 
+    let (col, row) = state.karel_pos in 
+    let (ncol, nrow) = 
+      (match state.karel_dir with 
+      | North -> (col, row - 1)
+      | South -> (col, row + 1)
+      | West -> (col - 1, row)
+      | East -> (col + 1, row))
+    in
+    (match get_cell state.grid (ncol, nrow) with
+    | None -> true
+    | Some x -> x = cell)  
+  | Facing dir -> state.karel_dir = dir
+  | Not pred' -> not (eval_pred state pred')
+  | NoBeepersPresent -> get_cell state.grid state.karel_pos <> Some Beeper
 
 
 let rec step (state : state) (code : instruction) : state =
-  raise Unimplemented
+  (* raise Unimplemented *)
+  match code with
+  | Move -> 
+    let (col, row) = state.karel_pos in
+    let (ncol, nrow) = 
+      (match state.karel_dir with 
+      | North -> (col, row - 1)
+      | South -> (col, row + 1)
+      | West -> (col - 1, row)
+      | East -> (col + 1, row))
+    in
+    (match get_cell state.grid (ncol, nrow) with
+    | None -> state
+    | Some x -> 
+      if x = Wall then 
+        state
+      else 
+        {state with karel_pos = (ncol, nrow)})
+
+  | TurnLeft -> 
+    (match state.karel_dir with
+    | North -> {state with karel_dir = West}
+    | West -> {state with karel_dir = South}
+    | South -> {state with karel_dir = East}
+    | East -> {state with karel_dir = North})
+  | PickBeeper -> 
+    let (col, row) = state.karel_pos in
+    (match get_cell state.grid (col, row) with
+    | Some Beeper -> {state with grid = set_cell state.grid (col, row) Empty} 
+    | _ -> state
+    )
+  | PutBeeper ->
+  let (col, row) = state.karel_pos in
+  (match get_cell state.grid (col, row) with
+  | Some Empty -> {state with grid = set_cell state.grid (col, row) Beeper} 
+  | _ -> state
+  )
+  | While (pred, instrs) -> 
+    if (eval_pred state pred) then 
+      step_list state (instrs @ [While (pred, instrs)])
+    else
+      state
+  | If (pred, then_, else_) ->
+    if (eval_pred state pred) then 
+      step_list state then_
+    else 
+      step_list state else_ 
+
+
 
 and step_list (state : state) (instrs : instruction list) : state =
   List.fold instrs ~init:state ~f:(fun state instr ->
@@ -117,4 +194,65 @@ and step_list (state : state) (instrs : instruction list) : state =
 ;;
 
 let checkers_algo : instruction list = [
+While ((Not (FrontIs Wall)) , [
+While ((Not (FrontIs Wall)) ,
+[
+ PutBeeper; Move; Move;	
+]
+);
+
+TurnLeft; TurnLeft;
+Move;
+If ((NoBeepersPresent), [ TurnLeft; TurnLeft; Move; PutBeeper], [TurnLeft; TurnLeft; Move]);
+TurnLeft; TurnLeft; TurnLeft;
+
+If ((Not (FrontIs Wall)),
+[
+Move;
+TurnLeft; TurnLeft; TurnLeft;
+
+While ((Not (FrontIs Wall)) ,
+[
+ Move;	
+]
+);
+
+TurnLeft; TurnLeft;
+
+While ((Not (FrontIs Wall)) ,
+[
+ Move; PutBeeper; Move;	
+]
+);
+
+TurnLeft; TurnLeft;
+Move;
+If ((NoBeepersPresent), [ TurnLeft; TurnLeft; Move; PutBeeper], [TurnLeft; TurnLeft; Move]);
+TurnLeft; TurnLeft; TurnLeft;
+
+If ((Not (FrontIs Wall)), 
+[
+Move;
+TurnLeft; TurnLeft; TurnLeft;
+
+While ((Not (FrontIs Wall)) ,
+[
+ Move;	
+]
+);
+
+TurnLeft; TurnLeft;
+], []);
+
+], 
+[]);
+
+]);
+
+If ((Facing East), [TurnLeft; TurnLeft] , [If ((Facing North), [TurnLeft], [ If ((Facing South), [TurnLeft; TurnLeft; TurnLeft], [])]) ]);
+While ((Not (FrontIs Wall)) ,
+[
+ Move;	
+]
+);
 ]
--- assign3/program/src/ngram_impl.ml
+++ assign3/program/src/ngram_impl.ml
@@ -7,22 +7,51 @@ type ngram_map = (ngram, string list) Map.Poly.t
 type word_distribution = float String.Map.t
 
 let rec remove_last_impl1 (l : string list) : string list =
-  raise Unimplemented
+  match l with 
+  | [] -> []
+  | x :: [] -> []
+  | x :: l' -> x :: remove_last_impl1 l' 
 ;;
 
 assert (remove_last_impl1 ["a"; "b"] = ["a"]);
+assert (remove_last_impl1 ["a"] = []);
+assert (remove_last_impl1 [] = []);
+assert (remove_last_impl1 ["1"; "1"; "4"; "5"; "1"; "4"] = ["1"; "1"; "4"; "5"; "1"]);
 ;;
 
 let remove_last_impl2 (l : string list) : string list =
-  raise Unimplemented
+  List.filteri l ~f:(fun (i : int) (n : string) : bool -> (i <> List.length l - 1))
 ;;
 
-assert (remove_last_impl2 ["a"; "b"] = ["a"])
+assert (remove_last_impl2 ["a"; "b"] = ["a"]);
+assert (remove_last_impl2 ["a"] = []);
+assert (remove_last_impl2 [] = []);
+assert (remove_last_impl2 ["1"; "1"; "4"; "5"; "1"; "4"] = ["1"; "1"; "4"; "5"; "1"]);
 ;;
 
 let compute_ngrams (l : string list) (n : int) : string list list =
-  raise Unimplemented
+  let len = List.length l in
+  let rec ngram (l : string list) (n: int) : string list = 
+    let len = List.length l in
+    match l with 
+    | [] -> []
+    | _ when n <= 0 -> []
+    | sublist when List.length sublist < n -> []
+    | x :: sublist -> x :: ngram sublist (n - 1)
+  in 
+  let (ans, _) = List.fold_left l ~init:([], l) ~f:(fun (accum, l') s -> 
+    let ng = ngram l' n in
+    if List.length ng < n then 
+      (accum, l')
+    else 
+      match List.tl l' with 
+      | None -> (ng :: accum, [])
+      | Some t1 -> (ng :: accum, t1))
+  in
+  List.rev ans
 ;;
+
+compute_ngrams ["a"; "b"; "c"] 2;;
 
 assert (compute_ngrams ["a"; "b"; "c"] 2 = [["a"; "b"]; ["b"; "c"]]);
 ;;
@@ -36,7 +65,12 @@ let ngram_map_new () : ngram_map =
 ;;
 
 let ngram_map_add (map : ngram_map) (ngram : ngram) : ngram_map =
-  raise Unimplemented
+  (* raise Unimplemented *)
+  let len = List.length ngram in
+  let (k, d) = List.split_n ngram (len - 1) in 
+  match Map.Poly.find map k with 
+  | None -> Map.Poly.set map ~key:k ~data:d
+  | Some x -> Map.Poly.set map ~key:k ~data:(x @ d)
 ;;
 
 let () =
@@ -48,7 +82,19 @@ let () =
 
 let ngram_map_distribution (map : ngram_map) (ngram : ngram)
   : word_distribution option =
-  raise Unimplemented
+  (* raise Unimplemented *)
+  let v = 
+    match Map.Poly.find map ngram with 
+    | None -> []
+    | Some x -> x
+  in
+  if v = [] then 
+    None
+  else
+    let len : int = List.length v in
+    let nlist = List.map ~f:(fun (s: string) -> (s, ((float_of_int 1) /. (float_of_int len)))) v in
+    let prob = String.Map.of_alist_reduce nlist (+.) in
+    Some(prob)
 ;;
 
 let distribution_to_string (dist : word_distribution) : string =
@@ -56,9 +102,45 @@ let distribution_to_string (dist : word_distribution) : string =
 ;;
 
 let sample_distribution (dist : word_distribution) : string =
-  raise Unimplemented
+  (* raise Unimplemented *)
+  let rnd = Random.float 1.0 in 
+  let (r, rs) = String.Map.fold dist ~init:(rnd, "") ~f:(fun ~key ~data (r, outs) -> 
+    if r < data then (1. , key)
+    else (r -. data, outs))
+  in
+  rs
+;;
+
+let () =
+  let map = ngram_map_new () in
+  let map = ngram_map_add map ["a"; "b"] in
+  let map = ngram_map_add map ["A"; "B"; "C"] in
+  let map = ngram_map_add map ["A"; "B"; "C"] in
+  let map = ngram_map_add map ["A"; "B"; "D"] in
+  let map = ngram_map_add map ["A"; "B"; "Vivek"] in
+    let v = match Map.Poly.find map ["A"; "B"] with
+		  | None -> []
+		  | Some x -> x
+	in
+  let dist = ngram_map_distribution map ["A"; "B"] in
+  match dist with
+  | None -> ()
+  | Some x -> 
+  let sdist = sample_distribution x in
+  (*Printf.printf "%s" sdist;
+  Printf.printf "%s" (distribution_to_string x);
+	Printf.printf "%s" (ngram_to_string v);*)
+	()
 ;;
 
 let rec sample_n (map : ngram_map) (ng : ngram) (n : int) : string list =
-  raise Unimplemented
+  (* raise Unimplemented *)
+  if n = 0 then []
+  else match Map.Poly.find map ng with
+  | None -> []
+  | Some x -> 
+    let dist = (ngram_map_distribution map ng) in 
+    match dist with
+    | None -> []
+    | Some x -> let s = sample_distribution x in s :: sample_n map ((remove_last_impl2 (List.rev ng)) @ [s]) (n - 1)
 ;;
